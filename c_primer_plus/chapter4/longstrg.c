2bde5dd3b88f15c0be12eabca738b85c2f6ef98e
yangshuhao1217 <yangshuhao017@gmail.com>
2021-10-05 17:06:48 +1000

    print()function

---
+++ c_primer_plus/chapter4/longstrg.c
@@ -0,0 +1,14 @@
+/* longstrg.c -- print long strings */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("here's one way to print a ");
+	printf("long string.\n");
+	printf("here's another way to print a \
+	long string.\n");
+	printf("Here's the newest way to print a "
+	"long string.\n"); /* ansi c */
+	return 0;
+}
