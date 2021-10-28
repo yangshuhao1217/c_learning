e3728ae6bb2b9c852c2dc478078e19661840e61f
yangshuhao1217 <yangshuhao017@gmail.com>
2021-10-28 16:44:36 +1000

    chapter6

---
+++ c_primer_plus/chapter6/entry.c
@@ -0,0 +1,23 @@
+/* entry.c -- exit-condition loop */
+
+#include <stdio.h>
+
+int main(void)
+{
+	const int secret_code = 13;
+	int code_entered ;
+
+	printf("To enter the trikaidekaphobia therapy club,\n");
+	printf("please enter the secret code number: ");
+	scanf("%d", &code_entered);
+
+	while (code_entered != secret_code)
+	{
+		printf("To enter the triakaidekaphobia therapy club,\n");
+		printf("please enter the secret code number: ");
+		scanf("%d", &code_entered);
+	}
+	printf("Congratulations! You are cured!\n");
+
+	return 0;
+}
