ef1ca1c16f04c273a09251fc31ae7a46b8662ff7
yangshuhao1217 <yangshuhao017@gmail.com>
2021-12-28 14:37:36 +0300

    chapter11

---
+++ c_primer_plus/chapter11/addresses.c
@@ -0,0 +1,17 @@
+// adresses.c  -- address of string.
+#define MSG "I'm special"
+
+
+#include <stdio.h>
+int main()
+{
+	char ar[] = MSG;
+	const char *pt = MSG;
+	printf("address of \"I'm special\": %p \n", "I'm special");
+	printf("		address ar: %p\n", ar);
+	printf("		address pt: %p\n", pt);
+	printf("		address of MSG: %p\n", MSG);
+	printf("address of \"I'm special\": %p \n", "I'm special");
+
+	return 0;
+}
