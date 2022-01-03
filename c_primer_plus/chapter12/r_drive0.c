06d182f038576447e8ebab4e6707434139819e5a
yangshuhao1217 <yangshuhao017@gmail.com>
2022-01-03 10:56:39 +0300

    chapter12

---
+++ c_primer_plus/chapter12/r_drive0.c
@@ -0,0 +1,15 @@
+/* r_drive0.c -- test rand0() function.*/
+/* compile with rand0.c */
+#include <stdio.h>
+extern unsigned int rand0(void);
+
+int main(void)
+{
+	int count;
+
+
+	for (count = 0; count < 5; count++)
+		printf("%d\n", rand0());
+
+	return 0;
+}
